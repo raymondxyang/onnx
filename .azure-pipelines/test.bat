3b6950ba115c5b2db2a862daed04ecffa838d48b
Raymond Yang <zihao.yang@microsoft.com>
2018-10-17 16:04:57 -0700

    try

--- .azure-pipelines/test.bat
+++ .azure-pipelines/test.bat
@@ -1,4 +1,5 @@
-dir /s /b /a-d "onnx_gtests.exe" >UT.txt & set /p _UT= < UT.txt & %_UT%
+dir /s /b /a-d "onnx_gtests.exe" >UT.txt & set /p _UT= < UT.txt
+%_UT%
 dir /b /a-d "*.whl" >WheelFile.txt & set /p _wheel= < WheelFile.txt
 pip install %_wheel%
 pytest
